c75e495a63c5c343978dccc84209adb317cd538e
WNguyen14 <wnguyen1@ufl.edu>
2022-02-03 14:43:14 -0500

    added new functionality

--- Lexer.java
+++ Lexer.java
@@ -1,6 +1,7 @@
 package edu.ufl.cise.plc;
 
 import edu.ufl.cise.plc.IToken.Kind;
+
 import java.util.ArrayList;
 
 public class Lexer implements ILexer{
@@ -21,6 +22,7 @@ public class Lexer implements ILexer{
 		int pos = 0;
 		int startPos = 0;
 		int len = 0;
+		ArrayList<Token> tokens = new ArrayList<Token>(); //arraylist to store tokens?
 		State state = State.START;
 		while (true)
 		{
@@ -37,12 +39,15 @@ public class Lexer implements ILexer{
 					}
 					case '+' -> 
 					{
-						Token newToken = new Token(Kind.PLUS, Character.toString(ch) ,startPos, 1); //WHERE AM I STORING THE TOKENS??
+						Token newToken = new Token(Kind.PLUS, "" ,startPos, 1); 
+						//we don't need to put the real input value in the token, we can derive it from startPos and the length
+						tokens.add(newToken);
 						pos++;
 					}
 					case '*' -> 
 					{
-						Token newToken = new Token(Kind.TIMES, Character.toString(ch) ,startPos, 1); 
+						Token newToken = new Token(Kind.TIMES, "" ,startPos, 1); 
+						tokens.add(newToken);
 						pos++;
 					}
 					case '=' ->
@@ -50,6 +55,27 @@ public class Lexer implements ILexer{
 						state = State.HAVE_EQ; 
 						pos++;
 					}
+					case '-' ->
+					{
+						state = State.HAVE_MINUS;
+						pos++;
+					}
+					case '1','2','3','4','5','6','7','8','9' ->
+					{
+						state = State.IN_NUM;
+						pos++;
+					}
+					case '0' ->
+					{
+						state = State.HAVE_ZERO;
+						pos++;
+					}
+					case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','$','_'  ->
+					{
+						state = State.IN_IDENT;
+						pos++;
+					}
+					
 					case 0 ->
 					{
 						// instructions say to add an EOF token?
@@ -65,8 +91,10 @@ public class Lexer implements ILexer{
 				{
 					case '=' ->
 					{
-						Token newToken = new Token(Kind.EQUALS, Character.toString(ch), startPos, 2);
+						Token newToken = new Token(Kind.EQUALS, "", startPos, 2);
+						tokens.add(newToken);
 						pos++;
+						state = State.START;
 					}
 					default->
 					{
@@ -90,13 +118,95 @@ public class Lexer implements ILexer{
 					}
 					default ->
 					{
-						Token newToken = new Token(Kind.INT_LIT, numbers.toString(), tokenPos, pos-tokenPos);
+						Token newToken = new Token(Kind.INT_LIT,"", tokenPos, pos-tokenPos);
+						tokens.add(newToken);
 						state = State.START;
 					}
 				}
+			}
+			else if (state == State.HAVE_MINUS)
+			{
+				switch (ch)
+				{
+				case '>' ->
+				{
+					Token newToken = new Token(Kind.RARROW, "", startPos, 2);
+					tokens.add(newToken);
+					pos++;
+					state = State.START;
+
+				}
+				default ->
+				{
+					//throw exception
+					state = State.START;
+					return;
+				}
 				
+				}
 				
 			}
+			else if (state == State.HAVE_ZERO)
+			{
+				switch(ch)
+				{
+				case '.' ->
+				{
+					state = State.HAVE_DOT;
+				}
+				default ->
+				{
+					Token newToken = new Token(Kind.INT_LIT, "", startPos, 1);
+					tokens.add(newToken);
+					pos++;
+					state = State.START;
+
+				}
+				}
+			}
+			else if (state == State.HAVE_DOT)
+			{
+				int tokenPos = 0;
+				ArrayList<Character> numbers = new ArrayList<Character>(); //store the numbers as we go along in this character array
+				switch (ch)
+				{
+				case '0','1','2','3','4','5','6','7','8','9' ->
+				{
+					tokenPos++;
+					pos++;
+					numbers.add(ch);
+				}
+				default ->
+				{
+					Token newToken = new Token(Kind.FLOAT_LIT, "", tokenPos, pos-tokenPos);
+					tokens.add(newToken);
+					state = State.START;
+				}
+				}
+			}
+			else if (state == State.IN_IDENT)
+			{
+				int tokenPos = 0;
+				//ArrayList<Character> numbers = new ArrayList<Character>(); //store the numbers as we go along in this character array
+				switch(ch)
+				{
+					
+					case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','$','_','0','1','2','3','4','5','6','7','8','9'  ->
+					{
+						tokenPos++;
+						pos++;
+						//numbers.add(ch);
+					}
+					default ->
+					{
+						Token newToken = new Token(Kind.IDENT,"", tokenPos, pos-tokenPos);
+						tokens.add(newToken);
+						state = State.START;
+					}
+				}
+			}
+			
+			
 		}
 	}
 	
