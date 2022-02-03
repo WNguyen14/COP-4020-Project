ed7e76a4d2b6b1676b521134090d76e6c5946137
WNguyen14 <73918319+WNguyen14@users.noreply.github.com>
2022-02-03 17:29:31 -0500

    Add files via upload

--- Lexer.java
+++ Lexer.java
@@ -2,13 +2,9 @@ package edu.ufl.cise.plc;
 
 import edu.ufl.cise.plc.IToken.Kind;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Lexer implements ILexer{
-	private ArrayList<IToken> tokens = new ArrayList<>();
-	private Kind kind;
-	private int internalPos = 0;
 	private enum State
 	{
 		START,
@@ -18,17 +14,17 @@ public class Lexer implements ILexer{
 		IN_FLOAT,
 		IN_NUM,
 		HAVE_EQ,
-		HAVE_MINUS,
+		HAVE_MINUS
 	}
-
+	
 	public Lexer(String input)
 	{
 		int pos = 0;
 		int startPos = 0;
-		int line = 0;
-		int col = 0;
+		int len = 0;
+		ArrayList<Token> tokens = new ArrayList<Token>(); //arraylist to store tokens?
 		State state = State.START;
-		while (pos < input.length())
+		while (true)
 		{
 			char[] chars = input.toCharArray();
 			char ch = chars [pos];
@@ -37,75 +33,57 @@ public class Lexer implements ILexer{
 				startPos = pos;
 				switch(ch) //issue: if there is no 0, then we are stuck?
 				{
-					case '\n' ->
-					{
-						line++;
-						col = 0;
-						pos++;
-					}
-					case ' ', '\t', '\r' ->
+					case ' ', '\t', '\n', '\r' -> 
 					{
 						pos++;
-						col++;
 					}
-					case '+' ->
+					case '+' -> 
 					{
-						Token newToken = new Token(Kind.PLUS, Character.toString(ch) ,new IToken.SourceLocation(line, col), 1); //WHERE AM I STORING THE TOKENS??
+						Token newToken = new Token(Kind.PLUS, "" ,startPos, 1); 
+						//we don't need to put the real input value in the token, we can derive it from startPos and the length
 						tokens.add(newToken);
 						pos++;
-						col++;
 					}
-					case '-' ->
-							{
-								Token newToken = new Token(Kind.MINUS, Character.toString(ch) ,new IToken.SourceLocation(line, col), 1); //WHERE AM I STORING THE TOKENS??
-								tokens.add(newToken);
-								pos++;
-								col++;
-							}
-					case '*' ->
+					case '*' -> 
 					{
-						Token newToken = new Token(Kind.TIMES, Character.toString(ch) ,new IToken.SourceLocation(line, col), 1);
+						Token newToken = new Token(Kind.TIMES, "" ,startPos, 1); 
 						tokens.add(newToken);
 						pos++;
-						col++;
 					}
 					case '=' ->
 					{
-						state = State.HAVE_EQ;
+						state = State.HAVE_EQ; 
 						pos++;
-						col++;
 					}
-					case '#' ->
+					case '-' ->
 					{
-						while(ch != '\n')
-						{
-							pos++;
-							col++;
-							ch = chars [pos];
-						}
+						state = State.HAVE_MINUS;
+						pos++;
 					}
-					case '"' ->
-							{
-								state = State.IN_IDENT;
-								kind = Kind.STRING_LIT;
-							}
+					case '1','2','3','4','5','6','7','8','9' ->
+					{
+						state = State.IN_NUM;
+						pos++;
+					}
+					case '0' ->
+					{
+						state = State.HAVE_ZERO;
+						pos++;
+					}
+					case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','$','_'  ->
+					{
+						state = State.IN_IDENT;
+						pos++;
+					}
+					
 					case 0 ->
 					{
 						// instructions say to add an EOF token?
 						return;
 					}
-					default ->
-					{
-						if ( ('a' <= ch) && (ch <= 'z') || ('A' <= ch) && (ch <= 'Z')  || ch == '_' || ch == '$') {
-							state = State.IN_IDENT;
-						}
-						else
-						{
-							System.out.println("unrecognized character");
-						}
-					}
+				
 				}
-
+				
 			}
 			else if (state == State.HAVE_EQ)
 			{
@@ -113,10 +91,10 @@ public class Lexer implements ILexer{
 				{
 					case '=' ->
 					{
-						Token newToken = new Token(Kind.EQUALS, Character.toString(ch), new IToken.SourceLocation(line, col), 2);
+						Token newToken = new Token(Kind.EQUALS, "", startPos, 2);
 						tokens.add(newToken);
 						pos++;
-						col++;
+						state = State.START;
 					}
 					default->
 					{
@@ -131,56 +109,106 @@ public class Lexer implements ILexer{
 				ArrayList<Character> numbers = new ArrayList<Character>(); //store the numbers as we go along in this character array
 				switch(ch)
 				{
-
+					
 					case '0','1','2','3','4','5','6','7','8','9' ->
 					{
 						tokenPos++;
 						pos++;
-						col++;
 						numbers.add(ch);
 					}
 					default ->
 					{
-						Token newToken = new Token(Kind.INT_LIT, numbers.toString(), new IToken.SourceLocation(line, col), pos-tokenPos);
+						Token newToken = new Token(Kind.INT_LIT,"", tokenPos, pos-tokenPos);
 						tokens.add(newToken);
 						state = State.START;
 					}
 				}
+			}
+			else if (state == State.HAVE_MINUS)
+			{
+				switch (ch)
+				{
+				case '>' ->
+				{
+					Token newToken = new Token(Kind.RARROW, "", startPos, 2);
+					tokens.add(newToken);
+					pos++;
+					state = State.START;
 
+				}
+				default ->
+				{
+					//throw exception
+					state = State.START;
+					return;
+				}
+				
+				}
+				
+			}
+			else if (state == State.HAVE_ZERO)
+			{
+				switch(ch)
+				{
+				case '.' ->
+				{
+					state = State.HAVE_DOT;
+				}
+				default ->
+				{
+					Token newToken = new Token(Kind.INT_LIT, "", startPos, 1);
+					tokens.add(newToken);
+					pos++;
+					state = State.START;
 
+				}
+				}
+			}
+			else if (state == State.HAVE_DOT)
+			{
+				int tokenPos = 0;
+				ArrayList<Character> numbers = new ArrayList<Character>(); //store the numbers as we go along in this character array
+				switch (ch)
+				{
+				case '0','1','2','3','4','5','6','7','8','9' ->
+				{
+					tokenPos++;
+					pos++;
+					numbers.add(ch);
+				}
+				default ->
+				{
+					Token newToken = new Token(Kind.FLOAT_LIT, "", tokenPos, pos-tokenPos);
+					tokens.add(newToken);
+					state = State.START;
+				}
+				}
 			}
 			else if (state == State.IN_IDENT)
 			{
 				int tokenPos = 0;
-				ArrayList<Character> characters = new ArrayList<Character>(); //store the characters in identifier
-				if ( ('a' <= ch) && (ch <= 'z') || ('A' <= ch) && (ch <= 'Z')  || ch == '_' || ch == '$' || ('0' <= ch) && (ch <= '9'))
+				//ArrayList<Character> numbers = new ArrayList<Character>(); //store the numbers as we go along in this character array
+				switch(ch)
 				{
-					tokenPos++;
-					pos++;
-					col++;
-					characters.add(ch);
+					
+					case 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','$','_','0','1','2','3','4','5','6','7','8','9'  ->
+					{
+						tokenPos++;
+						pos++;
+						//numbers.add(ch);
+					}
+					default ->
+					{
+						Token newToken = new Token(Kind.IDENT,"", tokenPos, pos-tokenPos);
+						tokens.add(newToken);
+						state = State.START;
+					}
 				}
-				else
-				{
-					Token newToken = new Token(Kind.IDENT, characters.toString(), new IToken.SourceLocation(line, col), pos-tokenPos);
-					tokens.add(newToken);
-					state = State.START;
-				}
-
 			}
+			
+			
 		}
-		tokens.add(new Token(Kind.EOF, input, new IToken.SourceLocation(line,col), 0));
 	}
-	@Override
-	public IToken next() throws LexicalException
-	{
-		return tokens.get(internalPos++);
-	}
-
-	@Override
-	public IToken peek() throws LexicalException {
-		return tokens.get(internalPos);
-	}
-
+	
 
 }
